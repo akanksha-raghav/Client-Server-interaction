a1c53415042e497b34998d06b09632fe2189e5c4
Akanksha Raghav <44342357+akku15999@users.noreply.github.com>
2019-08-01 19:02:24 +0530

    Update and rename client.py to Server.py

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 #server coding
 s.bind(("127.0.0.1",5000))
 s.listen(5)
-conn,addr=s.accept()#server can have many clientshence conn is used
+conn,addr=s.accept()#server can have many clients hence conn is used
 print(addr)
 conn.send(b"Hey this is server")#send in bytes
 print(conn.recv(1024))
