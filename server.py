6654b007aeef5f86fac900948451a951f8b81f3e
Akanksha Raghav <44342357+akku15999@users.noreply.github.com>
2019-08-01 19:01:04 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+s.bind(("127.0.0.1",5000))
+s.listen(5)
+conn,addr=s.accept()
+print("You got connected from address",addr)
+while True:
+	a="Akanksha==>"+input("")
+	conn.send(bytes(a,encoding='utf-8'))
+	print(conn.recv(1024))
+	
